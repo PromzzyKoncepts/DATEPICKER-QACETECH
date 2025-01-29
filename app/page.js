f45400ccf998ea80a0afb788d9a4a385cc14e7e8
Promise Okechukwu <okechukwupromise638@gmail.com>
2025-01-29 11:46:57 +0100

    defined states and added comments

--- app/page.js
+++ app/page.js
@@ -1,6 +1,21 @@
-
+"use client";
+import { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 export default function Home() {
+  // here i defined state for start date
+  const [startDate, setStartDate] = useState(null);
+  // here i defined state for ending date
+  const [endDate, setEndDate] = useState(null);
+  // here i defined state for leaving the calendar open and not closed
+  const [open, setOpen] = useState(true);
+
+  const handleChange = (range) => {
+    const [start, end] = range;
+    setStartDate(start);
+    setEndDate(end);
+  };
   return (
     <div className="">
       
